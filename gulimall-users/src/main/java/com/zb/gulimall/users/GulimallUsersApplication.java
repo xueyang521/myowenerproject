ebd0bf0b9dfcd0979bd47c53a1221b2610c826c4
薛阳 <xueyang219@126.com>
2020-09-27 11:26:34 +0800

    项目初始结构创建

---
+++ gulimall-users/src/main/java/com/zb/gulimall/users/GulimallUsersApplication.java
@@ -0,0 +1,13 @@
+package com.zb.gulimall.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallUsersApplication.class, args);
+    }
+
+}
---
+++ gulimall-users/src/test/java/com/zb/gulimall/users/GulimallUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.gulimall.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
